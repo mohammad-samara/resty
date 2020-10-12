4f5aa9b53f4cda90eb3ce3939fe356c5818b4c0e
mohammad-samara <mohammad.ab.samara@gmail.com>
2020-10-12 15:46:17 +0300

    Updates

---
+++ precache-manifest.6b5aa150abdc33998413a03df358f379.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61d70ea3681425004ff827cdd2a33b4c",
+    "url": "/resty/index.html"
+  },
+  {
+    "revision": "0fbadf5b6ee27f077bad",
+    "url": "/resty/static/css/main.00121692.chunk.css"
+  },
+  {
+    "revision": "cf811d98e47b61297a0b",
+    "url": "/resty/static/js/2.4d8c92e1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/resty/static/js/2.4d8c92e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0fbadf5b6ee27f077bad",
+    "url": "/resty/static/js/main.f63ee8a6.chunk.js"
+  },
+  {
+    "revision": "a82df10f567bbeaa6ec2",
+    "url": "/resty/static/js/runtime-main.ea9bec24.js"
+  }
+]);
